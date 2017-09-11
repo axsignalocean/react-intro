7b625ba1c53c0818feeae5ec8304ce50a54df38a
Alexandre Xypas <a.xypas@signalocean.com>
2017-09-11 16:14:34 +0300

    Extract the code of the react demo

--- presentation/index.js
+++ presentation/index.js
@@ -26,6 +26,8 @@ import preloader from "spectacle/lib/utils/preloader";
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
+import App from "./react-demo"
+
 // Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
@@ -46,82 +48,6 @@ const theme = createTheme({
     primary: "Montserrat",
     secondary: "Helvetica"
   });
-
-const Li = ({ isDone = false, onClick, children }) => (
-  <li
-    className="clickable"
-    style={{ textDecoration: isDone ? "line-through" : "" }}
-    onClick={onClick}
-  >
-    {children}
-  </li>
-)
-
-const Counter = ({ value, onClick }) => (
-  <span
-    className="clickable"
-    onClick={onClick}
-  >
-    Count clicks: {value}
-  </span>
-)
-
-const INITIAL_STATE = {
-  counter: 0,
-  isDoneItems: [false, false, false]
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = INITIAL_STATE
-  }
-
-  handleLiClick = (index) => (e) => {
-    this.setState((prevState, props) => ({
-      counter: prevState.counter + 1,
-      isDoneItems: prevState.isDoneItems.map((isDone, i) => index === i ? !isDone : isDone)
-    }))
-  }
-
-  handleCounterClick = (e) => {
-    this.setState(INITIAL_STATE)
-  }
-
-  render() {
-    const resetButton = (
-      <div>
-        <button onClick={this.handleCounterClick}>Reset</button>
-      </div>
-    )
-
-    const list = (
-      <ul style={{ textAlign: "left", listStylePosition: "inside", padding: 0 }}>
-        {
-          this.state.isDoneItems.map(
-            (isDone, index) => (
-              <Li
-                key={index}
-                isDone={isDone}
-                onClick={this.handleLiClick(index)}
-              >
-                Item - {index + 1}
-              </Li>
-            )
-          )
-        }
-      </ul>
-    )
-    return (
-      <div>
-        {/* Always one root element */}
-        {list}
-        <Counter value={this.state.counter} />
-        {resetButton}
-      </div>
-    )
-  }
-}
 
 export default class Presentation extends React.Component {
   render() {
